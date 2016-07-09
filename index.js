daa96b74262e178abab72414ab11e54935ade3dd
Nick Troccoli <nicktroccoli28@gmail.com>
2016-07-09 14:47:13 -0700

    Renamed modules to components

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, browserHistory } from 'react-router'
 import routes from './modules/routes'
-import App from './modules/App'
-import About from './modules/About'
-import Repos from './modules/Repos'
-import Repo from './modules/Repo'
-import Home from './modules/Home'
 
 window.onload = function() {
     render(
