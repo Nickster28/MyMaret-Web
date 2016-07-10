4f520b836faaa7d39830fd7e46420ecdea127d87
Nick Troccoli <nicktroccoli28@gmail.com>
2016-07-10 01:57:16 -0700

    Added 404 redirect

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, browserHistory, IndexRedirect } from 'react-router'
+import { Router, Route, browserHistory, IndexRedirect, Redirect } from 'react-router'
 import Parse from 'Parse'
 import App from './components/App'
 import Analytics from './components/Analytics'
 import Login from './components/Login'
 import Newspaper from './components/Newspaper'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+
+Parse.User.current = function() { return true; }
 
 // Check if user has not logged in yet, and redirect to login page
 function requireLogin(nextState, replace) {
@@ -31,11 +35,15 @@ window.onload = function() {
   render((
 	  <Router history={browserHistory}>
 	   	<Route path="/" component={App}>
-	   		<IndexRedirect to="/analytics"/>
-	   		<Route path="login" component={Login} onEnter={checkLoginBypass}/>
-	   		<Route path="newspaper" component={Newspaper} onEnter={requireLogin}/>
-	   		<Route path="analytics" component={Analytics} onEnter={requireLogin}/>
+	   		<IndexRedirect to="/analytics" />
+	   		<Route path="login" component={Login} onEnter={checkLoginBypass} />
+	   		<Route component={Home}>
+	   			<Route path="newspaper" component={Newspaper} onEnter={requireLogin} />
+	   			<Route path="analytics" component={Analytics} onEnter={requireLogin} />
+	   		</Route>
 	   	</Route>
+	   	<Route path='404' component={NotFound} />
+	   	<Redirect from='*' to='/404' />
 	  </Router>
   ), document.getElementById('app'))
 }
