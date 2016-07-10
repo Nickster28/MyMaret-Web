0905450e1a8c3b262129f03bc3e39644596718cf
Nick Troccoli <nicktroccoli28@gmail.com>
2016-07-10 01:47:02 -0700

    Got login redirect working

--- components/App.js
+++ components/App.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default React.createClass({
   render() {
-    return <div>MyMaret</div>
+    return (
+    	<div>
+    		<h1>MyMaret</h1>
+    		{this.props.children}
+    	</div>
+    )
   }
 })
--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import { Router, Route, browserHistory, IndexRedirect } from 'react-router'
+import Parse from 'Parse'
 import App from './components/App'
+import Analytics from './components/Analytics'
+import Login from './components/Login'
+import Newspaper from './components/Newspaper'
+
+function requireLogin(nextState, replace) {
+	if (!Parse.User.current()) {
+		replace({
+			pathname: '/login'
+		});
+	}
+}
 
 window.onload = function() {
+	Parse.initialize("mymaret-api-prod");
+
   render((
 	  <Router history={browserHistory}>
 	   	<Route path="/" component={App}>
-	   		<IndexRoute component={Home}/>
-	   		<Route path="/login" component={LogIn}/>
-	   		<Route path="/newspaper" component={Newspaper}/>
+	   		<IndexRedirect to="/analytics"/>
+	   		<Route path="login" component={Login}/>
+	   		<Route path="newspaper" component={Newspaper} onEnter={requireLogin}/>
+	   		<Route path="analytics" component={Analytics} onEnter={requireLogin}/>
 	   	</Route>
 	  </Router>
   ), document.getElementById('app'))
