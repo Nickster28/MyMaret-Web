44456f9ff3ef8f8f85180e3b4959993cebb7143c
Nick Troccoli <nicktroccoli28@gmail.com>
2016-07-10 03:59:10 -0700

    Debugging

--- server.js
+++ server.js
@@ -6,24 +6,25 @@ var app = express()
 
 app.use(compression())
 
-// serve our static stuff like index.css
-app.use(express.static(path.join(__dirname, 'public')))
-
 // If running in prod, auto-redirect to https.
 // Thanks to http://jaketrent.com/post/https-redirect-node-heroku/
 if (app.get('env') == 'production') {
-    console.log("Enabling trust proxy");
-    app.enable('trust proxy');
+    app.set('trust proxy');
+    app.use(function(req, res, next) {
+        if (req.protocol != 'https') {
+            res.redirect("https://" + req.hostname + req.originalUrl);
+        } else {
+            next();
+        }
+    });
 }
+
+// serve our static stuff like index.css
+app.use(express.static(path.join(__dirname, 'public')))
 
 // send all requests to index.html so browserHistory in React Router works
 app.get('*', function (req, res) {
-    console.log("Request protocol = " + req.protocol);
-    if (app.get('env') == 'production' && req.protocol != 'https') {
-        res.redirect("https://" + req.hostname + req.originalUrl);
-    } else {
-        res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    }
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 var PORT = process.env.PORT || 8080
