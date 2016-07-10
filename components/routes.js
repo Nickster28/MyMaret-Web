bba616d707fbe0f38622adcdfbc094f565ebb149
Nick Troccoli <nicktroccoli28@gmail.com>
2016-07-10 01:16:47 -0700

    Cleaned out sample components, added Parse

--- components/routes.js
+++ components/routes.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
-import App from './App'
-import About from './About'
-import Repos from './Repos'
-import Repo from './Repo'
-import Home from './Home'
+import App from './App.js'
+
+function requireLogin(nextState, replace) {
+	
+}
 
 module.exports = (
   <Route path="/" component={App}>
-    <IndexRoute component={Home}/>
-    <Route path="/repos" component={Repos}>
-      <Route path="/repos/:userName/:repoName" component={Repo}/>
-    </Route>
-    <Route path="/about" component={About}/>
+  	<IndexRoute component={Home}/>
+  	<Route path="login" component={LogIn}/>
+  	<Route path="newspaper" component={Newspaper}/>
   </Route>
 )
