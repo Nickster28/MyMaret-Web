05198580ea57d42c470b78f6b0db4be49fd596ef
Nick Troccoli <nicktroccoli28@gmail.com>
2016-07-10 01:24:53 -0700

    Removed server rendering, since it's a bit too bleeding-edge, and because it's not clear how you'd handle user auth with the client/server rendering split

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, browserHistory } from 'react-router'
-import routes from './components/routes'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import App from './components/App'
 
 window.onload = function() {
-    render(
-	   <Router routes={routes} history={browserHistory}/>,
-	   document.getElementById('app')
-    )
+  render((
+	  <Router history={browserHistory}>
+	   	<Route path="/" component={App}>
+	   		<IndexRoute component={Home}/>
+	   		<Route path="/login" component={LogIn}/>
+	   		<Route path="/newspaper" component={Newspaper}/>
+	   	</Route>
+	  </Router>
+  ), document.getElementById('app'))
 }
--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-import React from 'react'
-import { renderToString } from 'react-dom/server'
-import { match, RouterContext } from 'react-router'
-import routes from './components/routes'
-
 var express = require('express')
 var path = require('path')
 var compression = require('compression')
@@ -29,53 +24,8 @@ if (app.get('env') == 'production') {
 
 // send all requests to index.html so browserHistory in React Router works
 app.get('*', function (req, res) {
-  match({ routes: routes, location: req.url }, (err, redirect, props) => {
-    // in here we can make some decisions all at once
-    if (err) {
-      // there was an error somewhere during route matching
-      res.status(500).send(err.message)
-    } else if (redirect) {
-      // we haven't talked about `onEnter` hooks on routes, but before a
-      // route is entered, it can redirect. Here we handle on the server.
-      res.redirect(redirect.pathname + redirect.search)
-    } else if (props) {
-      // if we got props then we matched a route and can render
-      const appHtml = renderToString(<RouterContext {...props}/>)
-      res.send(renderPage(appHtml))
-    } else {
-      // no errors, no redirect, we just didn't match anything
-      res.status(404).send('Not Found')
-    }
-  })
-})
-
-function renderPage(appHtml) {
-  return `
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-    <html xmlns="http://www.w3.org/1999/xhtml">
-
-    <head>
-      <link rel="icon" href="/favicon.ico">
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="description" content="">
-      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-      <link rel="stylesheet" href="/index.css">
-      <script type="text/javascript" src="/bundle.js"></script>
-
-      <title>MyMaret - Server Rendering</title>
-    </head>
-
-    <body>
-      <div id="app">${appHtml}</div>
-    </body>
-
-    </html>
-   `
-}
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 var PORT = process.env.PORT || 8080
 app.listen(PORT, function() {
