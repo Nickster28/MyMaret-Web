d0274281c3a56e4b3cc1d68cd60e278baa2b9c1e
Nick Troccoli <nicktroccoli28@gmail.com>
2016-07-10 22:16:03 -0700

    Removed stub

--- ParseWrapper.js
+++ ParseWrapper.js
@@ -3,6 +3,4 @@ import Parse from 'parse'
 Parse.initialize("mymaret-api-prod");
 Parse.serverURL = "http://www.mymaret-api-prod.herokuapp.com/parse";
 
-Parse.User.current = function() { return false; }
-
 export default Parse;
