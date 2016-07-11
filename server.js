a195bc3a8f0be23346b706e50ccdeaa8241c9df0
Nick Troccoli <nicktroccoli28@gmail.com>
2016-07-10 18:35:44 -0700

    Removed HTTPS check for now

--- server.js
+++ server.js
@@ -6,21 +6,21 @@ var app = express()
 
 app.use(compression())
 
+// // If running in prod, auto-redirect to https.
+// // Thanks to http://jaketrent.com/post/https-redirect-node-heroku/
+// if (app.get('env') == 'production') {
+//     app.set('trust proxy');
+//     app.use(function(req, res, next) {
+//         if (req.protocol != 'https') {
+//             res.redirect("https://" + req.hostname + req.originalUrl);
+//         } else {
+//             next();
+//         }
+//     });
+// }
+
 // serve our static stuff like index.css
 app.use(express.static(path.join(__dirname, 'public')))
-
-// If running in prod, auto-redirect to https.
-// Thanks to http://jaketrent.com/post/https-redirect-node-heroku/
-if (app.get('env') == 'production') {
-    app.set('trust proxy');
-    app.use(function(req, res, next) {
-        if (req.protocol != 'https') {
-            res.redirect("https://" + req.hostname + req.originalUrl);
-        } else {
-            next();
-        }
-    });
-}
 
 // send all requests to index.html so browserHistory in React Router works
 app.get('*', function (req, res) {
