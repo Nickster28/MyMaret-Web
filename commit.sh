b30896d288bbbb345fc57ff85222fec96811b79f
Nick Troccoli <nicktroccoli28@gmail.com>
2016-07-17 17:53:36 -0700

    Renamed commit script and made it also push/commit to staging and master

--- commit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-webpack
-echo -e "Enter commit message: "
-read
-git commit -a -m "$REPLY"
---
+++ commitAndPushAll.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+webpack
+git stash
+git checkout staging
+git stash apply
+echo -e "Enter commit message: "
+read
+git commit -a -m "$REPLY"
+git push origin staging
+git checkout master
+git merge staging
+git push origin master
+git checkout staging
