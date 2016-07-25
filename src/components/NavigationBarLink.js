bea28f6888ba2c4d9935a9ac8071df177c612e98
Nick Troccoli <nicktroccoli28@gmail.com>
2016-07-24 17:58:03 -0700

    Fixed problem where after selection navbar drawer wouldn't collapse

--- src/components/NavigationBarLink.js
+++ src/components/NavigationBarLink.js
@@ -1,17 +1,30 @@
-/* FUNCTION: NavigationBarLink
+/* CLASS: NavigationBarLink
  * ----------------------------
  * A simple wrapper around React Router's Link component to ensure
  * that the activeClassName for every Link is always set to "active".
  *
- * Note that this is just a function because NavigationBarLink has no
- * internal state - it is merely a function of props, so we can declare
- * it as a function instead of a class.
+ * Also attaches an onClick handler to collapse the navigation drawer
+ * on smaller screens, since that doesn't happen automatically.
  * ----------------------------
  */
 
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router";
 
-export default function NavigationBarLink(props) {
-	return <Link {...props} activeClassName="active"/>;
+export default class NavigationBarLink extends Component {
+	
+	/* Collapses the navigation drawer on smaller width screens.
+	 * This is needed because the drawer doesn't collapse, since it
+	 * is normally used in non-single-page-apps where the page will
+	 * refresh and the drawer state won't matter anymore.
+	 */
+	collapseNavigation() {
+		if (window.$(document).width() < 768) {
+			window.$("#nav-toggle").click();
+		}
+	}
+
+	render() {
+		return <Link {...this.props} onClick={this.collapseNavigation.bind(this)} activeClassName="active"/>;
+	}
 }
