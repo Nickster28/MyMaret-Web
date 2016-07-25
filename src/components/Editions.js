bb40c1ad1d54df4598d038c253249956f0e63df1
Nick Troccoli <nicktroccoli28@gmail.com>
2016-07-24 23:58:58 -0700

    Added editions dropdown - need to figure out how to left-adjust it... (maybe put in nav?)

--- src/components/Editions.js
+++ src/components/Editions.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import DocumentTitle from "react-document-title";
-import EditionsList from "./EditionsList";
+import EditionsDropdown from "./EditionsDropdown";
 import EditionDetailView from "./EditionDetailView";
 import Config from "../config";
 import "../stylesheets/Editions.css";
 
 export default class Editions extends Component {
+
+	createEdition() {
+		alert("New edition!");
+	}
+
   	render() {
     	return (
     		<DocumentTitle title={Config.APP_NAME + " | Editions"}>
@@ -13,20 +18,17 @@ export default class Editions extends Component {
     				<div className="page-header">
     					<div className="btn-toolbar pull-right">
     						<div className="btn-group">
-    							<button type="button" id="newEditionButton" className="btn btn-primary">Create New</button>
+    							<EditionsDropdown handleCreate={this.createEdition.bind(this)}/>
     						</div>
     					</div>
     					<h2>Editions</h2>
     				</div>
 
     				<div className="row">
-    					<div className="col-xs-3">
-    						<EditionsList />
-    					</div>
-    					<div className="col-xs-9">
+    					<div className="col-xs-12">
     						<EditionDetailView />
     					</div>
-  					</div>
+    				</div>
     			</div>
     		</DocumentTitle>
     	)
--- src/components/EditionsList.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default class EditionsList extends Component {
-  	render() {
-    	return (
-            <div>Hello</div>
-    	)
-  	}
-}
